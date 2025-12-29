84aadd511b9b99105ec565da885179f40e255d9c
Tharusha Dilhara <119704521+TDila@users.noreply.github.com>
2025-12-29 18:40:52 +0530

    improved ml models using random forest

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import joblib
 import os
 
@@ -14,14 +15,14 @@ y_time = df["hours"]
 
 priority_model = Pipeline([
     ("tfidf", TfidfVectorizer()),
-    ("reg", LogisticRegression(max_iter=1000))
+    ("reg", RandomForestClassifier(n_estimators=100, random_state=42))
 ])
 
 priority_model.fit(x, y_priority)
 
 time_model = Pipeline([
     ("tfidf", TfidfVectorizer()),
-    ("reg", LinearRegression())
+    ("reg", RandomForestRegressor(n_estimators=100, random_state=42))
 ])
 
 time_model.fit(x, y_time)
